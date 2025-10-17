9b605282fccbcbc6d121f92264244e718f061b41
muxammediyar <djumabaev003@gmail.com>
2025-10-18 03:52:19 +0500

    bg red login page

--- src/app/(main)/auth/login/page.tsx
+++ src/app/(main)/auth/login/page.tsx
@@ -6,7 +6,7 @@ import { LoginForm } from "../_components/login-form";
 export default function LoginV2() {
   return (
     <>
-      <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px] bg-red-400">
         <div className="space-y-2 text-center">
           <h1 className="text-3xl font-medium">Login to your account</h1>
           <p className="text-muted-foreground text-sm">Please enter your details to login.</p>
